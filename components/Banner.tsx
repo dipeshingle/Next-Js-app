794cf526efc4ae6dc8cc26a122495318924fe388
dipesh_ingle <dipeshingle1926@gmail.com>
2022-08-26 19:14:41 +0530

    ui

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -22,7 +22,7 @@ const Banner = ({ netflixOriginals }: Props) => {
 
   return (
     <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
-      <div className="-z-10 absolute top-0 left-0 h-[95vh] w-screen ">
+      <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
         <Image
           src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
           layout="fill"
--- components/Header.tsx
+++ components/Header.tsx
@@ -23,7 +23,7 @@ function Header() {
   }, []);
 
   return (
-    <header className={`${isScrolled && "bg-[#141414]"}`}>
+    <header className={`${isScrolled && "bg-[#141414]"} z-50`}>
       <div className="flex items-center space-x-2 md:space-x-10">
         <img
           src="https://rb.gy/ulxxee"
